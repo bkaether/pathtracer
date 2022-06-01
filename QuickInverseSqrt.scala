4e024be87ba6e9a5ef03dce700c738426fb59ad7
kelayamatoz <tzhao1309@gmail.com>
2022-05-31 18:05:10 -0700

    Tested rsqrt on multiple points.

--- QuickInverseSqrt.scala
+++ QuickInverseSqrt.scala
@@ -1,4 +1,5 @@
 import spatial.dsl._
+import scala.math
 
 // src: https://en.wikipedia.org/wiki/Fast_inverse_square_root
 @spatial object QuickInverseSqrt extends SpatialApp {
@@ -8,23 +9,35 @@ import spatial.dsl._
   def main(args: Array[String]): Unit = {
     val argOut = ArgOut[F]
     val dram = DRAM[F](I32(8))
-    setMem(dram, Array.fromSeq(Seq(0.15625, 1, 2, 3, 4, 5, 6, 7).map(m => m.to[F])))
+    val dramOut = DRAM[F](I32(8))
+    val dataSeq = Seq(0.15625, 1, 2, 3, 4, 5, 6, 7)
+    val data = Array.fromSeq(dataSeq.map(m => m.to[F]))
+
+    setMem(dram, data)
     Accel {
       val numberTestMem = SRAM[F](I32(8))
+      val numberTestMemOut = SRAM[F](I32(8))
       numberTestMem load dram
-      val number = numberTestMem(I32(0))
-      val threeHalves = 1.5.to[F]
-      val x2 = number * 0.5.to[F]
-      val y = number
-      val i = y.as[L]
-      val ii = 0x5f3759df.to[L] - i / 2.to[L] // Shift seems not working. Replacing with a FixDiv.
-      val yy = ii.as[F]
-      val resultNewtonIter0 = yy * (threeHalves - x2 * yy * yy)
-      val resultNewtonIter1 = resultNewtonIter0 * (threeHalves - x2 * resultNewtonIter0 * resultNewtonIter0)
-      argOut := resultNewtonIter1
-    }
+      Foreach(I32(8) by I32(1)) { iAddr =>
+        val number = numberTestMem(iAddr)
+        val threeHalves = 1.5.to[F]
+        val x2 = number * 0.5.to[F]
+        val y = number
+        val i = y.as[L]
+        val ii = 0x5f3759df.to[L] - i / 2.to[L] // Shift seems not working. Replacing with a FixDiv.
+        val yy = ii.as[F]
+        val resultNewtonIter0 = yy * (threeHalves - x2 * yy * yy)
+        val resultNewtonIter1 = resultNewtonIter0 * (threeHalves - x2 * resultNewtonIter0 * resultNewtonIter0)
+        numberTestMemOut(iAddr)  = resultNewtonIter1
+      }
 
-    println("GOLD = " + 1.0 / sqrt(0.15625) + ", accel_val = " + getArg(argOut))
-    println(getArg(argOut))
+      dramOut store numberTestMemOut
+    }
+    println("RESULT =")
+    printArray(getMem(dramOut))
+    println("GOLD =")
+
+    val gold = dataSeq.map{ case m => (1.0 / math.sqrt(m)).to[F] }
+    printArray(Array.fromSeq(gold))
   }
 }
